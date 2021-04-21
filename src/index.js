121aae586e257acbba8a57ed654ad86854bb122c
Sandro Gomes <sandrogomes101@yahoo.com>
2021-04-21 02:59:18 -0300

    remove unused code and rename conflict variables

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const client = new SkynetClient('https://siasky.net/');
 
-  const uploadFile = document.querySelector('#save-trigger');
+  const upload = document.querySelector('#save-trigger');
 
   const selectElement = document.querySelector("#mediaFile");
 
-  uploadFile.addEventListener('click', (async () => {
+  upload.addEventListener('click', (async () => {
     try {
       let file = document.getElementById("mediaFile").files[0];
-      let reader = new FileReader();
-      reader.readAsBinaryString(file);
       const { skylink } = await client.uploadFile(file);
       const skylinkUrl = await client.getSkylinkUrl(skylink);
       document.getElementById('mediaLink').href = skylinkUrl
       document.getElementById('mediaLink').text = skylinkUrl
-      setFileSkylink(skylinkUrl);
     } catch (error) {
       console.log(error)
     }
